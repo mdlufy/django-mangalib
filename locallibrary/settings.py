1d879389d57de5831a8e16a8d029be11204beca6
mdlufy <german.panov.2018@mail.ru>
2022-05-26 23:40:19 +0300

    delete debug condition

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,6 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
 DEBUG = env('DEBUG') != 'False'
 
 # Set hosts to allow any app on Heroku and the local testing URL
