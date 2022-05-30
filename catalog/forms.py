bcb852a578081cc7ae866df5103457f390be4c40
mdlufy <german.panov.2018@mail.ru>
2022-05-31 01:45:55 +0300

    changed variable name

--- catalog/forms.py
+++ catalog/forms.py
@@ -39,7 +39,7 @@ class UserRegistrationForm(forms.ModelForm):
         fields = ('username', 'first_name', 'email')
 
     def clean_password2(self):
-        cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
+        cleanedData = self.cleaned_data
+        if cleanedData['password'] != cleanedData['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
-        return cd['password2']
+        return cleanedData['password2']
