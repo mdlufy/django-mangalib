0f570db1d2116422948b7586a1e97126a50769fc
mdlufy <german.panov.2018@mail.ru>
2022-05-31 23:31:43 +0300

    env variables setup

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -35,10 +35,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
-#Add URL maps to redirect the base URL to our application
+# Add URL maps to redirect the base URL to our application
 # from django.views.generic import RedirectView
 # urlpatterns += [
 #     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
