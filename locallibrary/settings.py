fc6163313874493520030432e4bf87bb11bc069f
mdlufy <german.panov.2018@mail.ru>
2022-06-01 00:21:08 +0300

    added correct env variables names

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -17,9 +17,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 import os
 
-env = os.environ.Env()
+import environ
 
-os.environ.Env.read_env()
+env = environ.Env()
+
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
