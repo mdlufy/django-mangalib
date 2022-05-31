48702a273238040c391b51de4c85dede54fceeab
mdlufy <german.panov.2018@mail.ru>
2022-06-01 00:04:17 +0300

    config env

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 import os
 
-env = os.environ.Env()
-
-os.environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
