403feacd8a54cdb53b1e9a8ed5e28066cdf303d6
mdlufy <german.panov.2018@mail.ru>
2022-06-01 17:33:33 +0300

    update allowed hosts to release

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 DEBUG = os.environ.get('DJANGO_DEBUG')
 
 # Set hosts to allow any app on Heroku and the local testing URL
-ALLOWED_HOSTS = ['boiling-lake-99724.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['boiling-lake-99724.herokuapp.com']
 
 
 # Application definition
