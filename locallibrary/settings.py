383d3f111a29e5c75ca4dc8544a7bd3ee06e8c6e
mdlufy <german.panov.2018@mail.ru>
2022-06-02 03:51:29 +0300

    cosmetic

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -17,7 +17,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 import os
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
